5d8a56cddea36454eda28a7504cfbe5a8d08e080
TuGatita777 <maxi.gonzalezg@duocuc.cl>
2025-04-22 21:53:52 -0400

    Upload CIP para Git

---
+++ inventory/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+#logueo
+def login(request):
+    return render(request, 'inventory/inicio/login.html')
+
+def home(request):
+    return render(request, 'inventory/inicio/home.html')
+#registro
+def registrar_usuario(request):
+    if request.method == 'POST':
+        # Aquí luego procesaremos los datos
+        pass
+    return render(request, 'inventory/inicio/registro.html')
+#inventario
+def inventario_gestion(request):
+    return render(request, 'inventory/gestion/inventario.html')
